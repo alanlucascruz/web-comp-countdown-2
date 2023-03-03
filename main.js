28f3e63ebb62c89ff573c198a8de48e18559ad78
Alan Cruz <alanlucascruz@gmail.com>
2023-03-02 23:18:56 -0300

    Formatação de Texto - PadStart adicionado

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const output = document.querySelector("h1");
 
 const countDown = () => {
-  const yourDate = new Date("2023-12-31");
+  const yourDate = new Date("2023-03-04");
 
   const countDownDate = new Date(yourDate).getTime();
   const now = new Date().getTime();
   const distance = countDownDate - now;
 
   const dd = Math.floor(distance / (1000 * 60 * 60 * 24));
-
   const hh = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
   const mm = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
   const ss = Math.floor((distance % (1000 * 60)) / 1000);
 
-  output.innerText = `${dd}d ${hh}h ${mm}m ${ss}s`;
+  const ddFormatted = String(dd).padStart(2, "0");
+  const hhFormatted = String(hh).padStart(2, "0");
+  const mmFormatted = String(mm).padStart(2, "0");
+  const ssFormatted = String(ss).padStart(2, "0");
+
+  output.innerText = `${ddFormatted}d ${hhFormatted}h ${mmFormatted}m ${ssFormatted}s`;
 };
 
+countDown();
 setInterval(countDown, 1000);
